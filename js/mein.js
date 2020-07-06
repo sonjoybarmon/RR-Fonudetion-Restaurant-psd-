03a8fc995516d1daffdf8dff655127dd13c89d34
Sonjoy Barmon <60999976+sonjoybarmon@users.noreply.github.com>
2020-07-06 15:30:58 +0600

    done

--- js/mein.js
+++ js/mein.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $(".header_area").sticky({topSpacing:0});
+    $(".header_area").sticky({topSpacing:0});  /*sticky menu end now */
+
+    $('.parallax_bg').parallaxie({
+        speed: 0.7,
+    }); /*parallaxie background end now */
+
 });
